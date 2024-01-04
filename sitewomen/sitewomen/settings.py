45bc582e805256bdb7bd8fe16f71d24d65d5cffb
semyon <semen.ryaboff@yandex.ru>
2024-01-04 12:45:02 +0300

    GET and POST requests. Request Exception Handlers

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-xe%j77wj4th4kt3e!-^=r+i5rjl_656xe5ebp&(n+a7r-m=$7u"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 
 
 def index(request):
@@ -10,8 +10,15 @@ def categories(request, cat_id):
 
 
 def categories_by_slug(request, cat_slug):
+    if request.GET:
+        print(request.GET)
     return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>")
 
 
 def archive(request, year):
+    if year > 2024:
+        raise Http404("")
     return HttpResponse(f"<h1>Архив по годам</h1><p >{year}</p>")
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
