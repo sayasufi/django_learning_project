808bd36b5b45664bf28886a20d4db4663231aee5
semyon <semen.ryaboff@yandex.ru>
2024-01-04 15:54:08 +0300

    Template inheritance (extends). The include tag

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = "django-insecure-xe%j77wj4th4kt3e!-^=r+i5rjl_656xe5ebp&(n+a7r-m=$7u
 DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1"]
-
 
 # Application definition
 
@@ -55,7 +53,9 @@ ROOT_URLCONF = "sitewomen.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / 'templates',
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -70,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "sitewomen.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -80,7 +79,6 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -100,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -111,7 +108,6 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -25,7 +25,7 @@ def index(request):
 
 
 def about(request):
-    return render(request, 'women/about.html', {"title": "О сайте"})
+    return render(request, 'women/about.html', {"title": "О сайте", "menu": menu})
 
 
 def show_post(request, post_id):
