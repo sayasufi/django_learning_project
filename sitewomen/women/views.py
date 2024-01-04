1bdd6373cc83d1074422e4d963ccd17b3a5930a7
semyon <semen.ryaboff@yandex.ru>
2024-01-04 15:38:39 +0300

    Template url tag

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import redirect, render
-from django.urls import reverse
+from django.shortcuts import render
 
-menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+        ]
 
 data_db = [
     {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
@@ -25,21 +28,20 @@ def about(request):
     return render(request, 'women/about.html', {"title": "О сайте"})
 
 
-def categories(request, cat_id):
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>id: {cat_id}</p>")
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
 
 
-def categories_by_slug(request, cat_slug):
-    if request.GET:
-        print(request.GET)
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>")
+def addpage(request):
+    return HttpResponse("Добавление статьи")
 
 
-def archive(request, year):
-    if year > 2024:
-        uri = reverse("cats", args=("music",))
-        return redirect(uri)
-    return HttpResponse(f"<h1>Архив по годам</h1><p >{year}</p>")
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+
+def login(request):
+    return HttpResponse("Авторизация")
 
 
 def page_not_found(request, exception):
