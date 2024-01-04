c460ec3bb30782242a7b3794be4d08c720b913a1
semyon <semen.ryaboff@yandex.ru>
2024-01-04 12:57:37 +0300

    Redirects (redirect)

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import redirect
+from django.urls import reverse
 
 
 def index(request):
@@ -17,8 +19,10 @@ def categories_by_slug(request, cat_slug):
 
 def archive(request, year):
     if year > 2024:
-        raise Http404("")
+        uri = reverse("cats", args=("music",))
+        return redirect(uri)
     return HttpResponse(f"<h1>Архив по годам</h1><p >{year}</p>")
+
 
 def page_not_found(request, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
