657c91a35a237e53f4874de6cdd49dee8043fd02
semyon <semen.ryaboff@yandex.ru>
2024-01-04 12:32:01 +0300

    Dynamic URLs. Custom Converters

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
-from women import views
+from django.urls import path, register_converter
+
+from . import converters
+from . import views
+
+register_converter(converters.FourDigitYearConverter, 'year4')
 
 urlpatterns = [
     path("", views.index),
-    path("cats/", views.categories),
+    path("cats/<int:cat_id>/", views.categories),
+    path("cats/<slug:cat_slug>/", views.categories_by_slug),
+    path('archive/<year4:year>/', views.archive),
 ]
