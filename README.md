c63f64a745e5e0ba20ef65dee50676c4aa07dcad
semyon <semen.ryaboff@yandex.ru>
2024-01-07 15:45:29 +0300

    Adding records to the database table. django-extensions module

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ```python3 manage.py runserver```
 ### 2. Запуск сервера в режиме DEBUG = True с загрузкой static файлов
 ```python3 manage.py runserver --insecure```
+### 3. Запуск оболочки для добавления записей в БД
+```python manage.py shell_plus --print-sql```
+
--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -1,1 +1,8 @@
-# Create your models here.
+from django.db import models
+
+class Women(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
