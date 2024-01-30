4ed677f17ee71211572c277bab4284d4b9ff3ff6
sayasufi <semen.ryaboff@yandex.ru>
2024-01-30 12:42:42 +0300

    Custom fields and actions in the admin panel

--- sitewomen/women/admin.py
+++ sitewomen/women/admin.py
@@ -1,19 +1,41 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 
 from .models import Women, Category
 
 
 @admin.register(Women)
 class WomenAdmin(admin.ModelAdmin):
-    list_display = ("id", "title", "time_create", "is_published", "cat")
-    list_display_links = ("id", "title")
+    list_display = (
+        "title",
+        "time_create",
+        "is_published",
+        "cat",
+        "brief_info",
+    )
+    list_display_links = ("title",)
     ordering = ["-time_create", "title"]
-    list_editable = ("is_published", )
+    list_editable = ("is_published",)
     list_per_page = 5
+    actions = ["set_published", "set_draft"]
+
+    @admin.display(description="Краткое описание", ordering="content")
+    def brief_info(self, women: Women):
+        return f"Описание {len(women.content)} символов"
+
+    @admin.action(description="Опубликовать выбранные записи")
+    def set_published(self, request, queryset):
+        count = queryset.update(is_published=Women.Status.PUBLISHED)
+        self.message_user(request, f"Изменено {count} записей.")
+
+    @admin.action(description="Снять с публикации")
+    def set_draft(self, request, queryset):
+        count = queryset.update(is_published=Women.Status.DRAFT)
+        self.message_user(
+            request, f"{count} записей сняты с публикации.", messages.WARNING
+        )
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("id", "name")
     list_display_links = ("id", "name")
-
