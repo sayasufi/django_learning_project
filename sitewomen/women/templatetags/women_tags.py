3bdcb6473ee7e91492084ef71e546b88e86984d9
sayasufi <semen.ryaboff@yandex.ru>
2024-01-30 15:54:41 +0300

    Customize the appearance of the admin panel

--- sitewomen/women/templatetags/women_tags.py
+++ sitewomen/women/templatetags/women_tags.py
@@ -6,12 +6,16 @@ from women.models import Category, TagPost
 register = template.Library()
 
 
-@register.inclusion_tag('women/list_tags.html')
+@register.inclusion_tag("women/list_tags.html")
 def show_all_tags():
-    return {"tags": TagPost.objects.annotate(total=Count("tags")).filter(total__gt=0)}
+    return {
+        "tags": TagPost.objects.annotate(total=Count("tags")).filter(
+            total__gt=0
+        )
+    }
 
 
-@register.inclusion_tag('women/list_categories.html')
+@register.inclusion_tag("women/list_categories.html")
 def show_categories(cat_selected_id=0):
     cats = Category.objects.annotate(total=Count("posts")).filter(total__gt=0)
     return {"cats": cats, "cat_selected": cat_selected_id}
