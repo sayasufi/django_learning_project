97ffe74d8b0be761ccb9f417660cd99ce7b82db9
sayasufi <semen.ryaboff@yandex.ru>
2024-01-30 12:29:15 +0300

    Configuring the display of the list of articles in the admin panel

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -14,24 +14,25 @@ class Women(models.Model):
         DRAFT = 0, "Черновик"
         PUBLISHED = 1, "Опубликовано"
 
-    title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255, unique=True, db_index=True)
-    content = models.TextField(blank=True)
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255, verbose_name="Заголовок")
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="Slug")
+    content = models.TextField(blank=True, verbose_name="Текст статьи")
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    time_update = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
     is_published = models.BooleanField(
-        choices=Status.choices, default=Status.DRAFT
+        choices=tuple(map(lambda x: (bool(x[0]), x[1]), Status.choices)), default=Status.DRAFT, verbose_name="Статус"
     )
     cat = models.ForeignKey(
-        "Category", on_delete=models.PROTECT, related_name="posts"
+        "Category", on_delete=models.PROTECT, related_name="posts", verbose_name="Категория"
     )
-    tags = models.ManyToManyField("TagPost", blank=True, related_name="tags")
+    tags = models.ManyToManyField("TagPost", blank=True, related_name="tags", verbose_name="Теги")
     husband = models.OneToOneField(
         "Husband",
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
         related_name="wuman",
+        verbose_name="Муж",
     )
 
     objects = models.Manager()
@@ -53,8 +54,12 @@ class Women(models.Model):
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name="Категория")
     slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
 
     def __str__(self):
         return self.name
