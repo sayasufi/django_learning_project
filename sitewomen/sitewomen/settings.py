b0b71f90565a68346d050bab6df14d2193bb9591
semyon <semyon.ryaboff@yandex.ru>
2024-02-02 17:59:52 +0300

    Displaying uploaded images in an HTML document and the admin panel

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -122,10 +122,11 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-MEDIA_ROOT = BASE_DIR / "media"
+
