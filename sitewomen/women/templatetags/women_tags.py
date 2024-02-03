d75aba6a79f451b1270e152e9c4793d3f1c3eff5
semyon <semen.ryaboff@yandex.ru>
2024-02-03 16:46:12 +0300

    Template context processors

--- sitewomen/women/templatetags/women_tags.py
+++ sitewomen/women/templatetags/women_tags.py
@@ -2,9 +2,13 @@ from django import template
 from django.db.models import Count
 
 from women.models import Category, TagPost
+from women.utils import menu
 
 register = template.Library()
 
+@register.simple_tag
+def get_menu():
+    return menu
 
 @register.inclusion_tag("women/list_tags.html")
 def show_all_tags():
