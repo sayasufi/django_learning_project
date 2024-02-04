34687be43086424b5c219d9975ade1373d41e309
semyon <semen.ryaboff@yandex.ru>
2024-02-04 13:02:47 +0300

    LoginView, Logout View and AuthenticationForm

--- sitewomen/users/urls.py
+++ sitewomen/users/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from . import views
@@ -5,6 +6,6 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path("login/", views.login_user, name="login"),
-    path("logout/", views.logout_user, name="logout"),
+    path('login/', views.LoginUser.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
