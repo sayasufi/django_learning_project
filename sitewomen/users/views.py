63d5dc708be76da35af7472556912fe15e73afb6
semyon <semen.ryaboff@yandex.ru>
2024-02-04 14:09:35 +0300

    Registration of users through the presentation functions

--- sitewomen/users/views.py
+++ sitewomen/users/views.py
@@ -1,10 +1,7 @@
-from django.contrib.auth import logout
-from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView
-from django.http import HttpResponseRedirect
-from django.urls import reverse, reverse_lazy
+from django.shortcuts import render
 
-from users.forms import LoginUserForm
+from users.forms import LoginUserForm, RegisterUserForm
 
 
 class LoginUser(LoginView):
@@ -16,24 +13,14 @@ class LoginUser(LoginView):
     #     return reverse_lazy("home")
 
 
-# def login_user(request):
-#     if request.method == "POST":
-#         form = LoginUserForm(request.POST)
-#         if form.is_valid():
-#             cd = form.cleaned_data
-#             user = authenticate(
-#                 request, username=cd["username"], password=cd["password"]
-#             )
-#
-#             if user and user.is_active:
-#                 login(request, user)
-#                 return HttpResponseRedirect(reverse("home"))
-#     else:
-#         form = LoginUserForm()
-#
-#     return render(request, "users/login.html", {"form": form})
-
-
-# def logout_user(request):
-#     logout(request)
-#     return HttpResponseRedirect(reverse("users:login"))
+def register(request):
+    if request.method == "POST":
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)  # создание объекта без сохранения в БД
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            return render(request, 'users/register_done.html')
+    else:
+        form = RegisterUserForm()
+    return render(request, 'users/register.html', {'form': form})
