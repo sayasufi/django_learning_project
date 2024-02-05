08d781c5562f909f0478ddced63512b88e2f2662
semyon <semyon.ryaboff@yandex.ru>
2024-02-05 14:15:38 +0300

    Configuring the mail server using the SMTP protocol

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -140,4 +140,14 @@ AUTHENTICATION_BACKENDS = [
     "users.authentication.EmailAuthBackend",
 ]
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = "smtp.yandex.ru"
+EMAIL_PORT = 465
+EMAIL_HOST_USER = "djan9oproject@yandex.ru"
+EMAIL_HOST_PASSWORD = "ebzlryubfbfdyhyw"
+EMAIL_USE_SSL = True
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+EMAIL_ADMIN = EMAIL_HOST_USER
