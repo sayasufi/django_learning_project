cd0e05e794ae24f9a22decfdbd5baca38c1c8ef4
semyon <semyon.ryaboff@yandex.ru>
2024-02-05 14:53:42 +0300

    Permissions & Groups

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,5 +1,8 @@
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    PermissionRequiredMixin,
+)
 from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render, get_object_or_404
@@ -50,11 +53,14 @@ class ShowPost(DataMixin, DetailView):
         )
 
 
-class AddPage(LoginRequiredMixin, DataMixin, CreateView):
+class AddPage(
+    PermissionRequiredMixin, LoginRequiredMixin, DataMixin, CreateView
+):
     form_class = AddPostForm
     template_name = "women/addpage.html"
     title_page = "Добавление статьи"
     # login_url = "/login/"
+    permission_required = "women.add_women"
 
     def form_valid(self, form):
         w = form.save(commit=False)
@@ -62,14 +68,16 @@ class AddPage(LoginRequiredMixin, DataMixin, CreateView):
         return super().form_valid(form)
 
 
-class UpdatePage(DataMixin, UpdateView):
+class UpdatePage(PermissionRequiredMixin, DataMixin, UpdateView):
     model = Women
     fields = ["title", "content", "photo", "is_published", "cat"]
     template_name = "women/addpage.html"
     success_url = reverse_lazy("home")
     title_page = "Редактирование статьи"
+    permission_required = "women.change_women"
 
 
+@permission_required(perm="women.view_women", raise_exception=True)
 def contact(request):
     return HttpResponse("Обратная связь")
 
